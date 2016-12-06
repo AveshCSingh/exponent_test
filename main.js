e988aed0bb667cda1e283cda74f5e6b82921984a
Avesh Singh <aveshcsingh@gmail.com>
2016-12-05 19:51:47 -0800

    Basic app works

--- main.js
+++ main.js
@@ -4,14 +4,26 @@ import {
   StyleSheet,
   Text,
   View,
+		TouchableHighlight,
 } from 'react-native';
 
 class App extends React.Component {
+		_onPressButton() {
+			this.setState({count: this.state.count + 1})
+		}
+
+		constructor(props) {
+				super(props)
+				this.state = {count: 0};
+				this._onPressButton = this._onPressButton.bind(this, this.state.count);
+		}
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Can I have my phone back?</Text>
-      </View>
+						<View style={styles.container}>
+						<TouchableHighlight onPress={this._onPressButton}>
+						<Text>Nose goes here. {this.state.count}</Text>
+						</TouchableHighlight>
+						</View>
     );
   }
 }
